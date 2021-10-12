564e5f119ae9df7121696710b4aa3aa47f9c4b53
stanislavk <stanislavk@fornova.net>
2021-10-13 00:32:05 +0300

    tel API

--- task1.py
+++ task1.py
@@ -6,4 +6,12 @@
 * для візуалізації можна за допомогою Mathplotlib згенерувати графік/діагарамку загруженості ресурсів  та надсилати повідомлення з картинкою
 """
 
-import requests
+import requests as r
+import psutil
+
+API_LINK = "https://api.telegram.org/bot2081315834:AAHFuHw9VSsgj-VWKnc9aVT5GewWt4bgeBY"
+updates_from_human = r.get("".join([API_LINK, "/getUpdates"])).json()
+message_from_bot = updates_from_human["result"][0]["message"]
+chat_id = message_from_bot["from"]["id"]
+text = message_from_bot["text"]
+sent_message = r.get(f"{API_LINK}/sendMessage?chat_id={chat_id}&text=hi, you wrote {text}")
