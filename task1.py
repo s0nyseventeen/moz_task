28c7304eb3aebe04deb5ac895748ba1b9a097183
stanislavk <stanislavk@fornova.net>
2021-10-14 00:24:06 +0300

    working on inserting to db

--- task1.py
+++ task1.py
@@ -9,34 +9,55 @@
 import requests as r
 import psutil
 from time import sleep
-from random import randint
+from typing import List
+import sqlite3
+
+conn = sqlite3.connect("statistics.db")
+cursor = conn.cursor()
 
 
 def main(N: int):
     while True:
+        core_data = get_core_usage()
+        ram_data = get_ram_usage()
         sleep(N)
-        send_message(randint(0, 9))
+        send_message(core_data, ram_data)
 
 
-def send_message(data: str):
+def send_message(core_data: List[float], ram_data: float):
     API_LINK = "https://api.telegram.org/bot"
     TOKEN = "2081315834:AAHFuHw9VSsgj-VWKnc9aVT5GewWt4bgeBY"
     chat_id = "658316569"
-    url = f"{API_LINK}{TOKEN}/sendMessage?chat_id={chat_id}&text={data}"
+    url = f"{API_LINK}{TOKEN}/sendMessage?chat_id={chat_id}&text={core_data} RAM = {ram_data}%"
     results = r.get(url)
     return results.json()
 
 
-    
+def get_core_usage():
+    data = psutil.cpu_freq(percpu=True)
+    data_percentage = [round(x[0] / 100, 2) for x in data]
+    return data_percentage
 
-def get_cpu_ram():
-    pass
+
+def get_ram_usage():
+    return psutil.virtual_memory()[2]
 
 
+def insert_db(table: str, core_data: List[float], ram_data: float):
+    cursor.executemany(
+        f"INSERT INTO {table} "
+        f"({columns}) "
+        f"VALUES ({some_values})",
+        values)
+    conn.commit()
 
 
 if __name__ == "__main__":
-    main(5)
+    main(3)
+    #print(get_core_usage())
+    #print(get_ram_usage())
+
+
 
 #updates_from_human = r.get("".join([API_LINK, TOKEN, "/getUpdates"])).json()
 #
